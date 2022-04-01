e72dc43dd803d8eed40651a2a58aa58eb689072f
Pascal Garber <pascal@artandcode.studio>
2022-04-01 12:21:25 +0200

    Ported MenuButton and shortcuts to GJS

--- main.ts
+++ main.ts
@@ -25,16 +25,88 @@ import GObject from './@types/Gjs/GObject-2.0.js'
 import Gio from './@types/Gjs/Gio-2.0.js'
 import GLib from './@types/Gjs/GLib-2.0.js'
 
+import { Window, MenuButton } from "./widgets.js";
 
-class MyWindowClass extends Gtk.Window {
+class _MyWindow extends Window {
 
+    revealer?: Gtk.Revealer;
+
+    override _init(config?: Gtk.ApplicationWindow_ConstructProps) {
+        super._init(config)
+        // load the custom css, so we can use it later
+        this.loadCSS('main.css');
+
+        // Add Menu Button to the titlebar (Right Side)
+        const menu = new MenuButton();
+        this.headerbar?.pack_end(menu);
+
+        // Create actions to handle menu actions
+        this.createAction('new', this.menuHandler.bind(this))
+        this.createAction('about', this.menuHandler.bind(this))
+        this.createAction('quit', this.menuHandler.bind(this))
+        this.createAction('shortcuts', this.menuHandler.bind(this))
+
+        // # make a new title label and add it to the left.
+        // # So we kan place the stack switcher in the middle
+        // label = Gtk.Label()
+        // label.set_text(title)
+        // # add 2 chars indent on the label for better looks
+        // label.set_halign(Gtk.Align.END)
+        // label.set_width_chars(len(title) + 2)
+        // this.headerbar.pack_start(label)
+        // # Main content box
+        // content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        // # Search Bar
+        // this.search = SearchBar(this)
+        // content.append(this.search)
+        // # search bar is active by default
+        // this.search.set_callback(this.on_search)
+
+        // # Stack
+        // this.stack = Stack()
+
+        // # Stack Page 1
+        // this.page1 = this.setup_page_one('page1', 'Page 1')
+        // # Stack Page 2
+        // this.page2 = this.setup_page_two('page2', 'Page 2')
+        // # Stack Page 3
+        // this.page3 = this.setup_page_three('page3', 'Page 3')
+        // # Stack Page 4
+        // this.page4 = this.setup_page_four('page4', 'Page 4')
+        // # Stack Page 5
+        // this.page5 = this.setup_page_five('page5', 'Page 5')
+        // # add stack switcher to center of titlebar
+        // this.headerbar.set_title_widget(this.stack.switcher)
+        // # Add stack to window
+        // content.append(this.stack)
+        // # Add main content box to window
+        // this.set_child(content)
+    }
+
+    showShortcuts() {
+        const builder = Gtk.Builder.new_from_file('shortcuts.ui')
+        const shortcuts = builder.get_object('shortcuts') as Gtk.ShortcutsWindow;
+        shortcuts.present()
+    }
+
+    // ---------------------- Handlers --------------------------
+
+    /** Callback for  menu actions */
+    menuHandler(action: Gio.SimpleAction) {
+        const name = action.get_name()
+        print(`active ${name}`)
+        if (name === 'quit')
+            this.close()
+        else if (name === 'shortcuts')
+            this.showShortcuts()
+    }
 }
 
 const MyWindow = GObject.registerClass({
     GTypeName: 'MyWindow'
-}, MyWindowClass );
+}, _MyWindow );
 
-class ApplicationClass extends Gtk.Application {
+class _Application extends Gtk.Application {
     _init(constructProperties = {
         application_id: 'dk.rasmil.Example',
         flags: Gio.ApplicationFlags.FLAGS_NONE
@@ -44,22 +116,26 @@ class ApplicationClass extends Gtk.Application {
 
     override vfunc_activate() {
         super.vfunc_activate();
+        print("vfunc_activate");
         let win = this.active_window;
         if (!win) {
+            print("MyWindow");
             win = new MyWindow({
                 title: "My Gtk4 Application",
                 default_width: 800,
-                default_height: 800
+                default_height: 800,
+                application: this
             })
         }
         win.present()
     }
+
 }
  
 /** Main Application class */
 const Application = GObject.registerClass({
     GTypeName: 'Application'
-}, ApplicationClass );
+}, _Application );
  
  /** Run the main application */
 const main = () => {
---
+++ widgets.ts
@@ -0,0 +1,124 @@
+import Gio from './@types/Gjs/Gio-2.0.js'
+import Gtk from './@types/Gjs/Gtk-4.0.js'
+import GObject from './@types/Gjs/GObject-2.0.js'
+
+import { APP_MENU } from "./constants.js";
+
+/**
+ * Wrapper class for at Gtk.Menubutton with a menu defined
+ * in a Gtk.Builder xml string
+ */
+class _MenuButton extends Gtk.MenuButton {
+
+    override _init(config: Gtk.MenuButton_ConstructProps = {}) {
+        const xml = APP_MENU;
+        const name = 'app-menu';
+        const icon_name = 'open-menu-symbolic';
+
+        super._init(config);
+        const builder = new Gtk.Builder();
+        builder.add_from_string(xml, xml.length)
+        const menu = builder.get_object(name) as Gio.MenuModel; // TODO: ts-for-gir: Use Generics here
+        this.set_menu_model(menu);
+        this.set_icon_name(icon_name);
+    }
+}
+
+export const MenuButton = GObject.registerClass({
+    GTypeName: 'MenuButton'
+}, _MenuButton );
+
+/** Wrapper for Gtk.Stack with  with a StackSwitcher */
+class _Stack extends Gtk.Stack {
+
+    public switcher?: Gtk.StackSwitcher;
+
+    private _pages: {[name: string]: Gtk.StackPage} = {};
+
+    override _init(config: Gtk.Stack_ConstructProps = {}) {
+        super._init(config);
+        this.switcher = new Gtk.StackSwitcher()
+        this.switcher.set_stack(this)
+    }
+
+
+    public add_page(name: string, title: string, widget: Gtk.Widget) {
+        const page = this.add_child(widget)
+        page.set_name(name)
+        page.set_title(title)
+        this._pages[name] = page
+        return page
+    }
+}
+
+export const Stack = GObject.registerClass({
+    GTypeName: 'Stack'
+}, _Stack );
+
+/** custom Gtk.ApplicationWindow with a headerbar */
+class _Window extends Gtk.ApplicationWindow {
+
+    /** custom CSS provider */
+    cssProvider?: Gtk.CssProvider;
+
+    headerbar?: Gtk.HeaderBar;
+
+    override _init(config: Gtk.ApplicationWindow_ConstructProps = {}) {
+        const title = config?.title || "";
+        delete config.title;
+        super._init(config);
+        
+        this.headerbar = new Gtk.HeaderBar();
+        this.set_titlebar(this.headerbar)
+        const label = new Gtk.Label()
+        label.set_text(title)
+        this.headerbar.set_title_widget(label)
+    }
+
+    /** create a provider for custom styling */
+    public loadCSS(path: string) {
+        print("loadCSS", path);
+        const file = Gio.File.new_for_path(path);
+        if (file.query_exists(null)) {
+            const cssProvider = new Gtk.CssProvider();
+            try {
+                cssProvider.load_from_path(path);
+            } catch (error) {
+                printerr(`Error loading CSS`, error)
+            }
+            print("loading custom styling")
+            this.get_style_context().add_provider(cssProvider, 1);
+            this.cssProvider = cssProvider;
+        } else {
+            printerr(`File ${path} not found!`)
+        }
+    }
+
+    private _addWidgetStyling(widget: Gtk.Widget) {
+        if (this.cssProvider) {
+            const context = widget.get_style_context();
+            context.add_provider(this.cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_USER);
+        }
+    }
+
+    public addCustomStyling(widget: Gtk.Widget) {
+        this._addWidgetStyling(widget);
+        // iterate children recursive
+        const children = widget.observe_children();
+        for (let i = 0; i < children.get_n_items(); i++) {
+            const child = children.get_item(i);
+            this.addCustomStyling(child as Gtk.Widget);
+        }
+    }
+
+    /** Add an Action and connect to a callback */
+    public createAction(name: string, callback: any) {
+        const action = Gio.SimpleAction.new(name, null);
+        action.connect("activate", callback);
+        this.add_action(action);
+    }
+}
+
+export const Window = GObject.registerClass({
+    GTypeName: 'Window'
+}, _Window );
